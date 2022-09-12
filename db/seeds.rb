ab1c1d9b291a562b50c832771679e52809cfaeb1
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-12 15:00:55 -0500

    Add messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Message.create(body: 'Hey this is the first message!')
+Message.create(body: 'Second message...')
+Message.create(body: 'React on Rails should be the name.')
+Message.create(body: 'I like trains.')
+Message.create(body: 'How does this even work?')
